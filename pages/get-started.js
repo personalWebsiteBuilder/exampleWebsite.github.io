25c1b5b5587155018944e533b98d8731b6676269
JD <medlin.j@husky.neu.edu>
2020-03-24 17:19:37 -0700

    added circular picture and body styling

--- pages/get-started.js
+++ pages/get-started.js
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import { Navbar } from '../components/NavBar'
 import { Container } from '../components/Container'
+import { CircularPicture } from '../components/CircularPicture'
 import { render } from 'react-dom'
 
 const getStarted = () => (
@@ -12,9 +13,27 @@ const getStarted = () => (
         <main>
             <Navbar />
             <Container>
+                <CircularPicture />
             </Container>
         </main>
         <style>{`
+        main {
+            padding: 5rem 0;
+            flex: 1;
+            display: flex;
+            flex-direction: column;
+            justify-content: center;
+            align-items: center;
+          }
+    
+          footer {
+            width: 100%;
+            height: 100px;
+            border-top: 1px solid #eaeaea;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+          }
         `}</style>
     </>
 );
